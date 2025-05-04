f9622febd21ff971f1a5772ee11627cd4639d862
instrutorvilson <instrutorvilsonmoro@gmail.com>
2025-05-04 18:33:39 -0300

    ajustes na forma de pagamento

--- src/main/java/controllers/FormaPagamentoController.java
+++ src/main/java/controllers/FormaPagamentoController.java
@@ -9,14 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dto.FormaPagamentoDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import models.FormaPagamento;
 
 @RestController
 @RequestMapping("/formapagamento")
+@Tag(name = "Forma de pagamento", description = "Operações relacionadas a forma de pagamento")
 public class FormaPagamentoController {
     FormaPagamentoDTO formaPagamentoDTO;
 
     @GetMapping
+    @Operation(summary = "Retorna todos as formas de pagamentos cadastradas")
     public List<FormaPagamentoDTO> getAllFormasPagamento() {
         return Arrays.stream(FormaPagamento.values())
                      .map(forma -> new FormaPagamentoDTO(forma.name(), forma.getDescricao()))
